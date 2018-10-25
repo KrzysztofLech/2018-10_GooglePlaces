644392150343011627dd764873c3ae0550c95264
Krzysztof Lech <krzysztof.lech@24abc.pl>
2018-10-25 08:51:42 +0200

    Initial Commit

---
+++ MoveApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoveApp
+//
+//  Created by Krzysztof Lech on 25/10/2018.
+//  Copyright © 2018 Krzysztof Lech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
